fc24b113d773c9afe691cad7560177665c088e42
Vitaly Bochkarev <vitalybochkarev@Book.local>
2022-08-24 09:44:52 +0700

    add sections templates App Header and HomeView

---
+++ frontend/src/components/AppHeader.vue
@@ -0,0 +1,15 @@
+<template>
+  <header class="header">
+    <div class="header__logo">
+      <a href="index.html" class="logo">
+        <img src="img/logo.svg" alt="V!U!E! Pizza logo" width="90" height="40">
+      </a>
+    </div>
+    <div class="header__cart">
+      <a href="cart.html">0 ₽</a>
+    </div>
+    <div class="header__user">
+      <a href="#" class="header__login"><span>Войти</span></a>
+    </div>
+  </header>
+</template>
