3886217a30e54c618f54bb15bcf81400c2981f79
YourfriendRND <kupeirc@gmail.com>
2023-03-05 12:13:47 +0300

    Добавил верстку в первые компоненты шапки и основной секции

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,14 +1,7 @@
 <template>
-  <div class="main__wrapper">
-    <div class="main__header">
-      <img src="@/assets/img/logo.svg" width="300" height="47" alt="V!U!E! Pizza" />
-    </div>
-    <h1>Добро пожаловать!</h1>
-    <p>
-      Это проект V!U!E! Pizza для обучения на профессиональном онлайн‑курсе<br />
-      <b>«Vue.js для опытных разработчиков».</b>
-    </p>
-  </div>
+  <app-layout>
+    <home-view />
+  </app-layout>
 </template>
 
 <style lang="scss">
@@ -60,3 +53,8 @@ body {
 }
 
 </style>
+
+<script setup>
+  import AppLayout from './layouts/AppLayout.vue';
+  import HomeView from './views/HomeView.vue';
+</script>
