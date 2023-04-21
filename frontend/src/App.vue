3c704d1ea1f6e8f194d1a40d5975c52b1bc184f9
pavel <shirokuiuna@mail.ru>
2023-04-21 19:06:12 +0300

    Добавление стилей в проекте Vue-Pizza

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -17,50 +17,10 @@
 </template>
 
 <style lang="scss">
-@import "@/assets/scss/app.scss";
+@import "src/assets/scss/style";
+
 body {
   justify-content: center;
   align-items: center;
-}
-.main__wrapper {
-  padding-bottom: 30px;
-
-  background-color: $white;
-  box-shadow: $shadow-light;
-
-  h1 {
-    margin-bottom: 0;
-    padding: 0 95px;
-
-    text-align: center;
-
-    @include b-s36-h42;
-  }
-
-  p {
-    padding: 0 95px;
-
-    text-align: center;
-
-    font-size: 20px;
-    line-height: 30px;
-  }
-
-  b {
-    font-size: 1.2em;
-  }
-}
-
-.main__header {
-  margin-bottom: 30px;
-  padding: 20px 0;
-
-  background-color: $green-600;
-
-  img {
-    display: block;
-
-    margin: 0 auto;
-  }
 }
 </style>
