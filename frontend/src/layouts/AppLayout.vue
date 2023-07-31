41ac2d16ce0cdc8f70bc45755b0c403672c8cbca
Ksenia <kseniarlog@gmail.com>
2023-07-31 16:43:30 +0300

    feat: layouts, views #1.20

---
+++ frontend/src/layouts/AppLayout.vue
@@ -0,0 +1,18 @@
+<template>
+  <app-header />
+  <slot />
+</template>
+
+<script>
+import { AppHeader } from "@/layouts";
+export default {
+  name: "AppLayout",
+  components: {
+    AppHeader,
+  },
+}
+</script>
+
+<style scoped>
+
+</style>
