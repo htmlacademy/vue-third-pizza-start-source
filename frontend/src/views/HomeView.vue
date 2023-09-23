8208bc8d41bcf88321f3b1d1f7966c1c37ca5ff8
konstantin-podolsckij <kpodolsckij@yandex.ru>
2023-09-23 17:23:24 +0300

    Поправил верстку в блоке выбора теста

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -17,9 +17,11 @@
                   class="visually-hidden"
                   checked
                 />
+                <img src="@/assets/img/dough-light.svg" alt="Тонкое тесто" />
                 <b>Тонкое</b>
                 <span>Из твердых сортов пшеницы</span>
               </label>
+
               <label class="dough__input dough__input--large">
                 <input
                   type="radio"
@@ -27,6 +29,7 @@
                   value="large"
                   class="visually-hidden"
                 />
+                <img src="@/assets/img/dough-large.svg" alt="Толстое тесто" />
                 <b>Толстое</b>
                 <span>Из твердых сортов пшеницы</span>
               </label>
@@ -509,199 +512,154 @@
 <style lang="scss">
 @import "@/assets/scss/ds-system/ds.scss";
 @import "@/assets/scss/mixins/mixins.scss";
-
 .content {
   padding-top: 20px;
 }
-
 .content__wrapper {
   display: flex;
   align-items: flex-start;
   flex-wrap: wrap;
-
   width: 920px;
   margin: 0 auto;
   padding-right: 2.12%;
   padding-bottom: 30px;
   padding-left: 2.12%;
 }
-
 .content__dough {
   width: 527px;
   margin-top: 15px;
   margin-right: auto;
   margin-bottom: 15px;
 }
-
 .content__diameter {
   width: 373px;
   margin-top: 15px;
   margin-bottom: 15px;
 }
-
 .content__ingredients {
   width: 527px;
   margin-top: 15px;
   margin-right: auto;
   margin-bottom: 15px;
 }
-
 .content__pizza {
   width: 373px;
   margin-top: 15px;
   margin-bottom: 15px;
 }
-
 .content__constructor {
   width: 315px;
   margin-top: 25px;
   margin-right: auto;
   margin-left: auto;
 }
-
 .content__result {
   display: flex;
   align-items: center;
   justify-content: center;
-
   margin-top: 25px;
-
   p {
     @include b-s24-h28;
-
     margin: 0;
   }
-
   button {
     margin-left: 12px;
     padding: 16px 45px;
   }
 }
-
 .sheet {
   padding-top: 15px;
-
   border-radius: 8px;
   background-color: $white;
   box-shadow: $shadow-light;
 }
-
 .sheet__title {
   padding-right: 18px;
   padding-left: 18px;
 }
-
 .sheet__content {
   display: flex;
   align-items: center;
   flex-wrap: wrap;
-
   margin-top: 8px;
   padding-top: 18px;
   padding-right: 18px;
   padding-left: 18px;
-
   border-top: 1px solid rgba($green-500, 0.1);
 }
-
 .ingredients__sauce {
   display: flex;
   align-items: center;
   flex-wrap: wrap;
-
   width: 100%;
   margin-bottom: 14px;
-
   p {
     @include r-s16-h19;
-
     margin-top: 0;
     margin-right: 16px;
     margin-bottom: 10px;
   }
 }
-
 .ingredients__input {
   margin-right: 24px;
   margin-bottom: 10px;
 }
-
 .ingredients__filling {
   width: 100%;
-
   p {
     @include r-s16-h19;
-
     margin-top: 0;
     margin-bottom: 16px;
   }
 }
-
 .title {
   box-sizing: border-box;
   width: 100%;
   margin: 0;
-
   color: $black;
-
   &--big {
     @include b-s36-h42;
   }
-
   &--small {
     @include b-s18-h21;
   }
 }
-
 .ingredients__list {
   @include clear-list;
-
   display: flex;
   align-items: flex-start;
   flex-wrap: wrap;
 }
-
 .ingredients__item {
   width: 100px;
   min-height: 40px;
   margin-right: 17px;
   margin-bottom: 35px;
 }
-
 .ingredients__counter {
   width: 54px;
   margin-top: 10px;
   margin-left: 36px;
 }
-
 .radio {
   cursor: pointer;
-
   span {
     @include r-s16-h19;
-
     position: relative;
-
     padding-left: 28px;
-
     &:before {
       @include p_center-v;
-
       display: block;
-
       box-sizing: border-box;
       width: 20px;
       height: 20px;
-
       content: "";
       transition: 0.3s;
-
       border: 1px solid $purple-400;
       border-radius: 50%;
       background-color: $white;
     }
   }
-
   &:hover {
     input:not(:checked):not(:disabled) + span {
       &:before {
@@ -709,16 +667,13 @@
       }
     }
   }
-
   input {
     display: none;
-
     &:checked + span {
       &:before {
         border: 6px solid $green-500;
       }
     }
-
     &:disabled {
       & + span {
         &:before {
@@ -726,7 +681,6 @@
           background-color: $silver-200;
         }
       }
-
       &:checked + span {
         &:before {
           border: 6px solid $purple-400;
@@ -735,218 +689,157 @@
     }
   }
 }
-
 .counter {
   display: flex;
-
   justify-content: space-between;
   align-items: center;
 }
-
 .counter__button {
   $el: &;
   $size_icon: 50%;
-
   position: relative;
-
   display: block;
-
   width: 16px;
   height: 16px;
   margin: 0;
   padding: 0;
-
   cursor: pointer;
   transition: 0.3s;
-
   border: none;
   border-radius: 50%;
   outline: none;
-
   &--minus {
     background-color: $purple-100;
-
     &::before {
       @include p_center-all;
-
       width: $size_icon;
       height: 2px;
-
       content: "";
-
       border-radius: 2px;
       background-color: $black;
     }
-
     &:hover:not(:active):not(:disabled) {
       background-color: $purple-200;
     }
-
     &:active:not(:disabled) {
       background-color: $purple-300;
     }
-
     &:focus:not(:disabled) {
       box-shadow: $shadow-regular;
     }
-
     &:disabled {
       cursor: default;
-
       &::before {
         opacity: 0.1;
       }
     }
   }
-
   &--plus {
     background-color: $green-500;
-
     &::before {
       @include p_center-all;
-
       width: $size_icon;
       height: 2px;
-
       content: "";
-
       border-radius: 2px;
       background-color: $white;
     }
-
     &::after {
       @include p_center-all;
-
       width: $size_icon;
       height: 2px;
-
       content: "";
       transform: translate(-50%, -50%) rotate(90deg);
-
       border-radius: 2px;
       background-color: $white;
     }
-
     &:hover:not(:active):not(:disabled) {
       background-color: $green-400;
     }
-
     &:active:not(:disabled) {
       background-color: $green-600;
     }
-
     &:focus:not(:disabled) {
       box-shadow: $shadow-regular;
     }
-
     &:disabled {
       cursor: default;
-
       opacity: 0.3;
     }
   }
-
   &--orange {
     background-color: $orange-200;
-
     &:hover:not(:active):not(:disabled) {
       background-color: $orange-100;
     }
-
     &:active:not(:disabled) {
       background-color: $orange-300;
     }
   }
 }
-
 .counter__input {
   @include r-s14-h16;
-
   box-sizing: border-box;
   width: 22px;
   margin: 0;
   padding: 0 3px;
-
   text-align: center;
-
   color: $black;
   border: none;
   border-radius: 10px;
   outline: none;
   background-color: transparent;
-
   &:focus {
     box-shadow: inset $shadow-regular;
   }
 }
-
 .dough__input {
   position: relative;
-
   margin-right: 8%;
   margin-bottom: 20px;
   padding-left: 50px;
-
   cursor: pointer;
-
   img {
     @include p_center-v;
-
     width: 36px;
     height: 36px;
-
     transition: 0.3s;
-
     border-radius: 50%;
   }
-
   b {
     @include r-s16-h19;
   }
-
   span {
     @include l-s11-h13;
-
     display: block;
   }
-
   &:hover {
     img {
       box-shadow: $shadow-regular;
     }
   }
-
   input {
     &:checked + img {
       box-shadow: $shadow-large;
     }
   }
 }
-
 .diameter__input {
   margin-right: 8.7%;
   margin-bottom: 20px;
   padding-top: 7px;
   padding-bottom: 6px;
-
   cursor: pointer;
-
   span {
     @include r-s16-h19;
-
     position: relative;
-
     padding-left: 46px;
-
     &::before {
       @include p_center_v;
-
       width: 36px;
       height: 36px;
-
       content: "";
       transition: 0.3s;
-
       border-radius: 50%;
       background-color: $green-100;
       background-image: url("@/assets/img/diameter.svg");
@@ -954,151 +847,117 @@
       background-position: center;
     }
   }
-
   &:nth-child(3n) {
     margin-right: 0;
   }
-
   &--small {
     span::before {
       background-size: 18px;
     }
   }
-
   &--normal {
     span::before {
       background-size: 29px;
     }
   }
-
   &--big {
     span::before {
       background-size: 100%;
     }
   }
-
   &:hover {
     span::before {
       box-shadow: $shadow-regular;
     }
   }
-
   input {
     &:checked + span::before {
       box-shadow: $shadow-large;
     }
   }
 }
-
 .filling {
   @include r-s14-h16;
-
   position: relative;
-
   display: block;
-
   padding-left: 36px;
-
   img {
     @include p_center-v;
-
     display: block;
-
     width: 32px;
     height: 32px;
-
     box-sizing: border-box;
     padding: 4px;
-
     border-radius: 50%;
   }
 }
-
 .button {
   $bl: &;
-
   @include b-s18-h21;
   font-family: inherit;
   display: block;
-
   box-sizing: border-box;
   margin: 0;
   padding: 0;
-
   cursor: pointer;
   transition: 0.3s;
   text-align: center;
-
   color: $white;
   border: none;
   border-radius: 8px;
   outline: none;
   box-shadow: $shadow-medium;
-
   background-color: $green-500;
-
   &:hover:not(:active):not(:disabled) {
     background-color: $green-400;
   }
-
   &:active:not(:disabled) {
     background-color: $green-600;
   }
-
   &:focus:not(:disabled) {
     opacity: 0.5;
   }
-
   &:disabled {
     background-color: $green-300;
     color: rgba($white, 0.2);
     cursor: default;
   }
-
   &--border {
     background-color: transparent;
     border: 1px solid $green-500;
     color: $black;
     box-shadow: none;
-
     &:hover:not(:active):not(:disabled) {
       color: $green-500;
       border-color: $green-500;
       background-color: transparent;
     }
-
     &:active:not(:disabled) {
       color: $green-600;
       border-color: $green-600;
       background-color: transparent;
     }
-
     &:disabled {
       opacity: 0.5;
     }
   }
-
   &--transparent {
     @include b-s14-h16;
     background-color: transparent;
     box-shadow: none;
     color: $black;
-
     &:hover:not(:active):not(:disabled) {
       color: $red-800;
       background-color: transparent;
     }
-
     &:active:not(:disabled) {
       color: $red-900;
       background-color: transparent;
     }
-
     &:disabled {
       opacity: 0.25;
     }
   }
-
   &--arrow {
     &::before {
       content: "";
@@ -1113,242 +972,186 @@
       transform: translateY(-1px);
     }
   }
-
   &--white {
     background-color: $white;
     color: $green-500;
   }
 }
-
 .pizza {
   position: relative;
-
   display: block;
-
   box-sizing: border-box;
   width: 100%;
-
   background-repeat: no-repeat;
   background-position: center;
   background-size: 100%;
-
   &--foundation--big-creamy {
     background-image: url("@/assets/img/foundation/big-creamy.svg");
   }
-
   &--foundation--big-tomato {
     background-image: url("@/assets/img/foundation/big-tomato.svg");
   }
-
   &--foundation--small-creamy {
     background-image: url("@/assets/img/foundation/small-creamy.svg");
   }
-
   &--foundation--small-tomato {
     background-image: url("@/assets/img/foundation/small-tomato.svg");
   }
 }
-
 .pizza__wrapper {
   width: 100%;
   padding-bottom: 100%;
 }
-
 .pizza__filling {
   $bl: &;
   position: absolute;
   top: 0;
   left: 0;
-
   display: block;
-
   width: 100%;
   height: 100%;
-
   background-repeat: no-repeat;
   background-position: center;
   background-size: 100%;
-
   &::before,
   &::after {
     display: none;
-
     position: absolute;
     top: 0;
     left: 0;
-
     width: 100%;
     height: 100%;
-
     content: "";
-
     background-image: inherit;
   }
-
   &--second {
     &::before {
       display: block;
-
       transform: rotate(45deg);
     }
   }
-
   &--third {
     &::before {
       display: block;
-
       transform: rotate(45deg);
     }
-
     &::after {
       display: block;
-
       transform: rotate(-45deg);
     }
   }
-
   &--ananas,
   &--ananas.pizza__filling--second::before,
   &--ananas.pizza__filling--third::after {
     background-image: url("@/assets/img/filling-big/ananas.svg");
   }
-
   &--bacon,
   &--bacon.pizza__filling--second::before,
   &--bacon.pizza__filling--third::after {
     background-image: url("@/assets/img/filling-big/bacon.svg");
   }
-
   &--blue_cheese,
   &--blue.pizza__filling--second::before,
   &--blue.pizza__filling--third::after {
     background-image: url("@/assets/img/filling-big/blue_cheese.svg");
   }
-
   &--cheddar,
   &--cheddar.pizza__filling--second::before,
   &--cheddar.pizza__filling--third::after {
     background-image: url("@/assets/img/filling-big/cheddar.svg");
   }
-
   &--chile,
   &--chile.pizza__filling--second::before,
   &--chile.pizza__filling--third::after {
     background-image: url("@/assets/img/filling-big/chile.svg");
   }
-
   &--ham,
   &--ham.pizza__filling--second::before,
   &--ham.pizza__filling--third::after {
     background-image: url("@/assets/img/filling-big/ham.svg");
   }
-
   &--jalapeno,
   &--jalapeno.pizza__filling--second::before,
   &--jalapeno.pizza__filling--third::after {
     background-image: url("@/assets/img/filling-big/jalapeno.svg");
   }
-
   &--mozzarella,
   &--mozzarella.pizza__filling--second::before,
   &--mozzarella.pizza__filling--third::after {
     background-image: url("@/assets/img/filling-big/mozzarella.svg");
   }
-
   &--mushrooms,
   &--mushrooms.pizza__filling--second::before,
   &--mushrooms.pizza__filling--third::after {
     background-image: url("@/assets/img/filling-big/mushrooms.svg");
   }
-
   &--olives,
   &--olives.pizza__filling--second::before,
   &--olives.pizza__filling--third::after {
     background-image: url("@/assets/img/filling-big/olives.svg");
   }
-
   &--onion,
   &--onion.pizza__filling--second::before,
   &--onion.pizza__filling--third::after {
     background-image: url("@/assets/img/filling-big/onion.svg");
   }
-
   &--parmesan,
   &--parmesan.pizza__filling--second::before,
   &--parmesan.pizza__filling--third::after {
     background-image: url("@/assets/img/filling-big/parmesan.svg");
   }
-
   &--salami,
   &---salami.pizza__filling--second::before,
   &---salami.pizza__filling--third::after {
     background-image: url("@/assets/img/filling-big/salami.svg");
   }
-
   &--salmon,
   &--salmon.pizza__filling--second::before,
   &--salmon.pizza__filling--third::after {
     background-image: url("@/assets/img/filling-big/salmon.svg");
   }
-
   &--tomatoes,
   &--tomatoes.pizza__filling--second::before,
   &--tomatoes.pizza__filling--third::after {
     background-image: url("@/assets/img/filling-big/tomatoes.svg");
   }
 }
-
 .input {
   display: block;
-
   span {
     @include r-s14-h16;
-
     display: block;
-
     margin-bottom: 4px;
   }
-
   input {
     @include r-s16-h19;
-
     display: block;
-
     box-sizing: border-box;
     width: 100%;
     margin: 0;
     padding: 8px 16px;
-
     transition: 0.3s;
-
     color: $black;
     border: 1px solid $purple-400;
     border-radius: 8px;
     outline: none;
     background-color: $white;
-
     font-family: inherit;
-
     &:focus {
       border-color: $green-500;
     }
   }
-
   &:hover {
     input {
       border-color: $black;
     }
   }
-
   &--big-label {
     display: flex;
     align-items: center;
-
     span {
       @include b-s16-h19;
-
       margin-right: 16px;
-
       white-space: nowrap;
     }
   }
