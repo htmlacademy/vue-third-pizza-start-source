ceaf51690e0daf4c05ab317c117bfcb7726db46d
Nikolai Shabalin <dev.nikolai.shabalin@gmail.com>
2024-12-04 11:04:00 +0300

    Исправляет работу препроцессора Sass

--- template/gulpfile.js
+++ template/gulpfile.js
@@ -11,7 +11,7 @@ const imagemin = require('gulp-imagemin');
 const svgmin = require('gulp-svgmin');
 const svgstore = require('gulp-svgstore');
 
-const sass = require('gulp-sass')(require('node-sass'));
+const sass = require('gulp-sass')(require('sass'));
 const postcss = require('gulp-postcss');
 const autoprefixer = require('autoprefixer');
 const objectFit = require('postcss-object-fit-images')
@@ -77,7 +77,8 @@ gulp.task('style', function () {
   return gulp.src('src/sass/style.scss')
     .pipe(plumber())
     .pipe(sass({
-      outputStyle: 'expanded'
+      outputStyle: 'expanded',
+      silenceDeprecations: ['legacy-js-api'],
     }))
     .pipe(postcss([
       autoprefixer(),
