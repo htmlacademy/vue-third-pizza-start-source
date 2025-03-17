3e6be5ad257ccae2e5ada4357b25805074d18ae9
Schailes <tuds@mail.ru>
2025-03-17 08:45:56 +0300

    2.6. Задание: общие компоненты в проекте Vue-Pizza

---
+++ frontend/src/common/components/AppDrag.vue
@@ -0,0 +1,30 @@
+<template>
+    <div
+      :draggable="true"
+      @dragstart.self="onDrag"
+      @dragover.prevent
+      @dragenter.prevent
+    >
+      <slot />
+    </div>
+  </template>
+
+<script setup>
+import { DATA_TRANSFER_PAYLOAD } from "@/common/constants";
+
+const props = defineProps({
+  draggable: {
+    type: Boolean,
+    default: false,
+  },
+  dataTransfer: {
+    type: Object,
+    required: true,
+  },
+});
+
+const onDragStart = ({ dataTransfer }) => {
+  const data = JSON.stringify(props.dataTransfer);
+  dataTransfer.setData(DATA_TRANSFER_PAYLOAD, data);
+};
+</script>
