f59b4818e4575ebf96054d3a882d34012ad4df36
Nick Gorbunov <intsurfer73@gmail.com>
2025-04-07 20:33:00 +0300

    DZ3.14

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,5 +1,5 @@
 import { createRouter, createWebHistory } from "vue-router";
-import { routes } from "@/router/routes";
+import { routes } from "@/router/routes.js";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
