34b6849044fef2bbb1cbf875a46025b732fadd88
Nick Gorbunov <intsurfer73@gmail.com>
2025-04-07 19:51:41 +0300

    DZ3.5

--- frontend/src/common/components/AppCounter.vue
+++ frontend/src/common/components/AppCounter.vue
@@ -63,19 +63,19 @@
   .counter__button {
     $el: &;
     $size_icon: 50%;
-    
+  
     position: relative;
-    
+  
     display: block;
-    
+  
     width: 16px;
     height: 16px;
     margin: 0;
     padding: 0;
-    
+  
     cursor: pointer;
     transition: 0.3s;
-    
+  
     border: none;
     border-radius: 50%;
     outline: none;
@@ -85,12 +85,12 @@
   
       &::before {
         @include p_center-all;
-        
+  
         width: $size_icon;
         height: 2px;
-        
+  
         content: "";
-        
+  
         border-radius: 2px;
         background-color: $black;
       }
@@ -121,44 +121,44 @@
   
       &::before {
         @include p_center-all;
-        
+  
         width: $size_icon;
         height: 2px;
-        
+  
         content: "";
-        
+  
         border-radius: 2px;
         background-color: $white;
       }
-    
+  
       &::after {
         @include p_center-all;
-        
+  
         width: $size_icon;
         height: 2px;
-        
+  
         content: "";
         transform: translate(-50%, -50%) rotate(90deg);
-        
+  
         border-radius: 2px;
         background-color: $white;
       }
-    
+  
       &:hover:not(:active):not(:disabled) {
         background-color: $green-400;
       }
-    
+  
       &:active:not(:disabled) {
         background-color: $green-600;
       }
-    
+  
       &:focus:not(:disabled) {
         box-shadow: $shadow-regular;
       }
-    
+  
       &:disabled {
         cursor: default;
-        
+  
         opacity: 0.3;
       }
     }
